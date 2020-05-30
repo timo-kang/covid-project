dce2c9553d849d8f89b9bde71f35be1182410183
kingworm <innovate57@kaist.ac.kr>
2020-05-30 20:45:16 +0900

    todos

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,3 +4,7 @@ var router = express.Router();
 router.get("/", function(req, res, next) {
   res.status(200).send("hello world!");
 });
+
+// TODO::client type returns
+// TODO::black boards contents returns
+// TODO::Classroom Alarms returns
